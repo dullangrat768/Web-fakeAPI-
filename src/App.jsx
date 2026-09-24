247a0ab522f4c148f224ee72a2c12a65f1456e9b
dullangrat768 <dullangratoficial@gmail.com>
2026-09-24 15:11:49 +0000

    Prueba 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import { BrowserRouter as Router, Route, Routes, Navigate,Link } from 'react-router-dom';
 import { useState } from 'react'
 import Inicio from './components/inicio'
 import Coleccion from './components/coleccion'
 import Favoritos from './components/favoritos'
 import Info from './components/info'
 import Usuario from './components/usuario'
-import Fakeapi from './components/fakeapi'
+import Pokemon from './components/pokemon';
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Inicio/>
-    <Coleccion/>
-    <Favoritos/>
-    <Info/>
-    <Usuario/>
-    <Fakeapi/>
+    <Router>
+      <nav className='c-menu'>
+        <Link to="/">Inicio</Link>
+        <Link to="/Coleccion">Coleccion</Link>
+        <Link to="/Favoritos">Favoritos</Link>
+        <Link to="/Info">Info</Link>
+        <Link to="/Usuario">Usuario</Link>
+      </nav>
+      <Routes>
+      <Route path='/' element={<Inicio/>}/>
+      <Route path='/Coleccion' element={<Coleccion/>} />
+      <Route path='/Favoritos' element={<Favoritos/>} />
+      <Route path='/Info' element={<Info/>} />
+      <Route path='/Usuario' element={<Usuario/>} />
+      <Route path="/pokemon/:name" element={<Pokemon />} />
+
+      </Routes>
+    </Router>
     </>
   )
 }
